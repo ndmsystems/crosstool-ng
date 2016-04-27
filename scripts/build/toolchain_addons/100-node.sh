634a98691779b14bc1a23398fe8cde2d7d360cfe
Eugene Yudin <e.yudin@ndmsystems.com>
2016-04-27 16:24:50 +0300

    Case 7565 (node): Use option "CT_STATIC_TOOLCHAIN" for static build.

--- scripts/build/toolchain_addons/100-node.sh
+++ scripts/build/toolchain_addons/100-node.sh
@@ -14,15 +14,23 @@ do_toolchain_addons_node_extract() {
 
 do_toolchain_addons_node_build() {
     CT_DoStep EXTRA "Installing node"
+    local -a extra_config
+
+    if [ "${CT_STATIC_TOOLCHAIN}" = "y" ]; then
+        extra_config+=("--fully-static")
+    fi
 
     CT_DoLog EXTRA "Copying sources to build dir"
     CT_DoExecLog ALL cp -av "${CT_SRC_DIR}/node-${CT_NODE_VERSION}" \
                             "${CT_BUILD_DIR}/build-node"
     CT_Pushd "${CT_BUILD_DIR}/build-node"
 
-    CT_DoExecLog CFG ./configure --prefix="${CT_PREFIX_DIR}"
+    CT_DoExecLog CFG            \
+    ./configure                 \
+    --prefix="${CT_PREFIX_DIR}" \
+    "${extra_config[@]}"
 
-    CT_DoExecLog ALL ${make} ${JOBSFLAGS} LDFLAGS=-static
+    CT_DoExecLog ALL ${make} ${JOBSFLAGS}
     CT_DoExecLog ALL ${make} install
     CT_Popd
     CT_EndStep
